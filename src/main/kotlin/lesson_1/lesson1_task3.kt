614e76ff9aa9e64512b6c1cf2906767e048ec8da
Vladimir Moiseenko <waldemar.moiseenko@gmail.com>
2025-04-01 09:10:58 +0300

    KS-1-3 variables created and output

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("$year\n$hour\n$minute")
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
+}
