08c8d70429b4c2dd4d5533bfb94ed5c46717e2fe
Vladimir Moiseenko <waldemar.moiseenko@gmail.com>
2025-04-02 08:28:45 +0300

    KS-1-3 changed int to string

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_1
 
 fun main() {
-    val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
 
     println("$year\n$hour\n$minute")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("$hour:$minute")
 }
